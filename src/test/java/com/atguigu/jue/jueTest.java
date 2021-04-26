7c6dcfe25fafcd6450980d183c1ac6ba1ac65c9e
谭思文 <OpenWisdomTsw@qq.com>
2021-04-26 19:43:37 +0800

    jue git12321123123

--- src/test/java/com/atguigu/jue/jueTest.java
+++ src/test/java/com/atguigu/jue/jueTest.java
@@ -36,6 +36,7 @@ public class jueTest {
         System.out.println("1sds11");
         System.out.println("acadfsaf");
         System.out.println("123456");
+        System.out.println("你是傻逼");
         Ticket ticket = new Ticket();
         new Thread(() -> { for (int i = 1; i <= 31; i++) ticket.sale(); }, "A").start();
         new Thread(() -> { for (int i = 1; i <= 31; i++) ticket.sale(); }, "B").start();
