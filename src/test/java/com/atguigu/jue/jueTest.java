0ba46904f59edf664f45ccf710b3d9a50aaa48bb
尹志鹏 <1978976138@qq.com>
2021-04-26 19:36:45 +0800

    bababaabab

--- src/test/java/com/atguigu/jue/jueTest.java
+++ src/test/java/com/atguigu/jue/jueTest.java
@@ -34,6 +34,7 @@ class Ticket {
 public class jueTest {
     public static void main(String[] args) {
         System.out.println("aaaaaaaaa");
+        System.out.println("bbbbbbbbbbb");
         Ticket ticket = new Ticket();
         new Thread(() -> { for (int i = 1; i <= 31; i++) ticket.sale(); }, "A").start();
         new Thread(() -> { for (int i = 1; i <= 31; i++) ticket.sale(); }, "B").start();
